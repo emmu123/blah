91c441fcd3241955e61164b4a1f89bbc515499ed
emmu123 <89377181+emmu123@users.noreply.github.com>
2023-11-05 20:48:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const webhookURL = "https://discord.com/api/webhooks/1170740313250340995/CCm33M7ne4MU2eI-K7iznANSSpmPyYG7FGQxZACIiWgZ9VNwK0yl5wWTATuHR-PVq7N-" // MANDATORY | Your Webhook URL Here
-const avatarURL = "https://indianforge.xyz/if.png" // OPTIONAL | The URL for the PFP of the webhook itself. Must be a link to a supported image format.
+const webhookURL = "https://discord.com/api/webhooks/1127178369428095056/uMtICCPo4ndiJkrKRBke0yvcT5Rsq0OUuAfZLwvK7bqARXrGbszTGxq1MsMdVeXDislY" // MANDATORY | Your Webhook URL Here
+const avatarURL = "https://noice.milcongift.repl.co/logo.png" // OPTIONAL | The URL for the PFP of the webhook itself. Must be a link to a supported image format.
 
 // Getting the elements from the HTML page
 const username = document.getElementById('username')
-const reason = document.getElementById('mail')
-const appeal = document.getElementById('issue')
+const usertag = document.getElementById('usertag')
+const userID = document.getElementById('userID')
+const reason = document.getElementById('reason')
+const appeal = document.getElementById('appeal')
 
 function sendMessage() { // After submitting the form.
     var request = new XMLHttpRequest() // Creates a new XML Http Request
@@ -16,21 +18,24 @@ function sendMessage() { // After submitting the form.
         title: "New Order!", // Title of the embed 
         timestamp: new Date(), // Footer Timestamp of the embed
         color: 0xFF0000, // Color of the embed
-        footer: { text: `made by dsc.gg/milcondev | User ID: ${(userID.value).toString()}` }, // Footer of the embed
+        footer: { text: `User ID: ${(userID.value).toString()}` }, // Footer of the embed
         fields: [ // Fields of the embed
-            { name: "User", value: `${username.value}#${(usertag.value).toString()}`, inline: true },
+            { name: "User", value: `${username.value}`, inline: true },
+
             { name: "Mail", value: `${mail.value}` },
-            { name: "Issue", value: `${issue.value}` }
+            { name: "Location", value: `${issue.value}` },
+          { name: "Age", value: `${age.value}` }
         ]
     }
 
     var params = { // Parameters to send the request
-        username: `Contact`, // Name of the webhook
+        username: `Pixy Share premium Order`, // Name of the webhook
         avatar_url: avatarURL, // PFP URL of the webhook
         embeds: [ embed ] // Embeds to send with the webhook
     }
 
     request.send(JSON.stringify(params)) // Sends the request 
-    alert('Your Message send to Our team') // Tells the user that the appeal has been sent.
-    console.log('Used sentSent!') // Logs that the order has been sent. (For Your server webhook)
+  alert('Order successfully sent!')
+    alert('Your Redeem code will recive with in 24 or 48hours. Need any help? Make ticket in discord Server. \n\n\ Join discord pixyshare.is-a.dev/discord') // Tells the user that the appeal has been sent.
+     // Logs that the order has been sent. (For Your server webhook)
 }
