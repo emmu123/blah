dc255512997451a8a12d5b65d2c99f1991d4a3c9
emmu123 <89377181+emmu123@users.noreply.github.com>
2023-11-06 13:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const webhookURL = "https://discord.com/api/webhooks/1127178369428095056/uMtICCPo4ndiJkrKRBke0yvcT5Rsq0OUuAfZLwvK7bqARXrGbszTGxq1MsMdVeXDislY" // MANDATORY | Your Webhook URL Here
-const avatarURL = "https://noice.milcongift.repl.co/logo.png" // OPTIONAL | The URL for the PFP of the webhook itself. Must be a link to a supported image format.
+const webhookURL = "https://discord.com/api/webhooks/1170740313250340995/CCm33M7ne4MU2eI-K7iznANSSpmPyYG7FGQxZACIiWgZ9VNwK0yl5wWTATuHR-PVq7N-" // MANDATORY | Your Webhook URL Here
+const avatarURL = "https://media.discordapp.net/attachments/950712799254941746/950720634881728562/newmilcon-1.png?width=586&height=586" // OPTIONAL | The URL for the PFP of the webhook itself. Must be a link to a supported image format.
 
 // Getting the elements from the HTML page
 const username = document.getElementById('username')
@@ -18,25 +18,23 @@ function sendMessage() { // After submitting the form.
         title: "New Order!", // Title of the embed 
         timestamp: new Date(), // Footer Timestamp of the embed
         color: 0xFF0000, // Color of the embed
-        footer: { text: `User ID: ${(userID.value).toString()}` }, // Footer of the embed
+        footer: { text: `made by dsc.gg/milcondev | User ID: ${(userID.value).toString()}` }, // Footer of the embed
         fields: [ // Fields of the embed
             { name: "User", value: `${username.value}#${(usertag.value).toString()}`, inline: true },
             { name: "UserID", value: `${(userID.value).toString()}`, inline: true },
-            { name: "Plan", value: `${reason.value}` },
-            { name: "Mail", value: `${mail.value}` },
-            { name: "Location", value: `${appeal.value}` },
-          { name: "Age", value: `${age.value}` }
+            { name: "Bot choosen", value: `${reason.value}` },
+            { name: "paid ?", value: `${appeal.value}` },
+           { name: "Ticket Ok", value: `${ticket.value}` }
         ]
     }
 
     var params = { // Parameters to send the request
-        username: `Pixy Share premium Order`, // Name of the webhook
+        username: `dsc.gg/milcondev`, // Name of the webhook
         avatar_url: avatarURL, // PFP URL of the webhook
         embeds: [ embed ] // Embeds to send with the webhook
     }
 
     request.send(JSON.stringify(params)) // Sends the request 
-  alert('Order successfully sent!')
-    alert('Your Redeem code will recive with in 24 or 48hours. Need any help? Make ticket in discord Server. \n\n\ Join discord pixyshare.is-a.dev/discord') // Tells the user that the appeal has been sent.
-     // Logs that the order has been sent. (For Your server webhook)
+    alert('Order Sent! Make Ticket and enjoy With new bot') // Tells the user that the appeal has been sent.
+    console.log('Order sentSent!') // Logs that the order has been sent. (For Your server webhook)
 }
